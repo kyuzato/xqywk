90c336c3a3099d1238ebd9ff762fc15ae538cb23
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-05-16 21:27:23 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,3 +1,8 @@
+sudo apt install git curl python3-pip ffmpeg -y
+pip3 install -U pip
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+sudo apt-get install -y nodejs
+npm i -g npm
 cd DevCatarina
-docker build . -t katarina
-docker run --name music katarina
+pip3 install -U -r requirements.txt
+python3 -m oda
