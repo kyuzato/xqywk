2cc3404b7bc4249a649914ea5a0cdb1312523fff
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-05-23 18:12:35 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,7 +1,5 @@
 git clone -b main https://github.com/AsmSafone/VideoPlayerBot musicdev
 cp musiccreds/.env musicdev/.env
 cd musicdev
-sudo apt-get install python3-pip ffmpeg
-pip3 install -U pip
-pip3 install -U -r requirements.txt
-python3 main.py
+docker build . -t catarinadev
+docker run catarinadev
